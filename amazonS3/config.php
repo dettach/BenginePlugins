ec37ccf51a6c671025d20d31d89f061c78fdbcf0
Bengine <info@bengine.ru>
2012-10-01 09:51:52 +0600

    Plugin Amazon S3

---
+++ amazonS3/config.php
@@ -0,0 +1,8 @@
+<?php
+	$plugin_config = array();
+	$plugin_config["admin"] = 1;
+	$plugin_config["name"] = "amazonS3";
+	$plugin_config["title"] = "amazonS3";
+	$plugin_config["extend"] = 1;
+	$plugin_config["cache"] = 0;
+?>
---
+++ amazonS3/plugin.php
@@ -0,0 +1,4 @@
+<?php
+if( !defined("BENGINE") ) { die ("Hacking!"); }
+include_once(ROOT_DIR."/plugins/amazonS3/S3.php");
+?>
