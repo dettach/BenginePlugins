b97b71f3c8880999bed52538af90a170ae239321
Bengine <info@bengine.ru>
2013-02-15 13:14:42 +0600

    fix

--- example/plugin.php
+++ example/plugin.php
@@ -16,12 +16,6 @@ if(isset($nodes[1]) and (int)$nodes[1] > 0) {
 # Смотрим список всех записей
 else
 {	
-	# Определяем сортировку
-	if(isset($plugin_config["order"]) and $plugin_config["order"] == true) {
-		$order = "DESC";
-	} else {
-		$order = "";
-	}
 	# Навигация по плагину
 	if(isset($plugin_config["limit"]) and $plugin_config["limit"] != 0) {	
 		$nav = donav($plugin_config["limit"], $pl, "page='".$page["id"]."'", $p);
@@ -29,8 +23,14 @@ else
 	} else {
 		$navigation = "";
 	}
+	# Сортировка по возрастанию или по убыванию
+	(!empty($plugin_config["order"]) and $plugin_config["order"] == true) ? $order = "DESC" : $order = "";
+	
+	# По какому полю производить сортировку
+	(!empty($plugin_config["sort"])) ?  $sort = $plugin_config["sort"] : $sort = "id";
+
 	# Запрашиваем весь массив данных
-	$sql = doquery("SELECT * FROM ".$pl." WHERE page='".$page["id"]."' ORDER BY `".$plugin_config["sort"]."` ".$order." ".$navigation);
+	$sql = doquery("SELECT * FROM ".$pl." WHERE page='".$page["id"]."' ORDER BY `".$sort."` ".$order." ".$navigation);
 	if(dorows($sql) > 0) {
 		$content = doarray($sql);
 	}
